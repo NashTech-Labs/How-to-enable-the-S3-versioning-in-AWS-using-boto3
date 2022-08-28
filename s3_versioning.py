b0de44e45fb72f6f23bd179e982a14a250ff2ba3
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 09:39:22 +0530

    Update in code

--- s3_versioning.py
+++ s3_versioning.py
@@ -1,14 +1,15 @@
+# MuZakkir Saifi
 # import the boto3 which will use to interact  with the aws
 
 import boto3
 
-AWS_REGION = input("Enter the AWS_REGION Name")
+REGION = input("Enter the REGION Name")
 BUCKET_NAME = input("Enter the bucket name")
 
-s3_resource = boto3.resource("s3", region_name=AWS_REGION)
+resource_for_s3 = boto3.resource("s3", region_name=REGION)
 
 def enable(bucket_name):
-    versioning = s3_resource.BucketVersioning(bucket_name)
+    versioning = resource_for_s3.BucketVersioning(bucket_name)
     versioning.enable()
 
     print(f'Wow, Your versioning is enabled for S3 Bucket: {versioning.status}')
